2d1dd2016c3e3141f064de1d8bef614731c86a1d
Palina <polina.sofina.20@mail.ru>
2024-09-05 11:39:33 +0500

    Add files via upload

--- module_3_4.py
+++ module_3_4.py
@@ -1,9 +1,7 @@
 def single_root_words(root_word, *other_words):
     same_words = []
-    root_word = root_word.lower()
     for i in other_words:
-        i = i.lower()
-        if i in root_word or root_word in i:
+        if i.lower() in root_word.lower() or root_word.lower() in i.lower():
             same_words.append(i)
     return same_words
 
